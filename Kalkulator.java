2e7689df74496e5d2d880c61355d5ccecb9b5cc6
Damian <Damian@example.com>
2026-04-27 09:33:11 +0200

    Dodano plik Kalkulator.java

---
+++ Kalkulator.java
@@ -0,0 +1,13 @@
+public class Kalkulator {
+  public int dodaj(int a, int b) {
+    return a + b;
+  }
+
+  public int dodaj(int a, int b, int c) {
+    return a + b + c;
+  }
+
+  public double dodaj(double a, double b) {
+    return a + b;
+  }
+}
